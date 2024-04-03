d441df4724b77c08e8f5d0af46c39855502bee1b
dahniardd <143977411+dahniardd@users.noreply.github.com>
2024-04-03 18:51:18 +0700

    Add files via upload
    Stack Main Pakaian

--- StackDemo06.java
+++ StackDemo06.java
@@ -2,15 +2,32 @@ import java.util.Scanner;
 
 public class StackDemo06 {
     public static void main (String[]args) {
-        Stack06 stack = new Stack06(10);
-        stack.push (8);
-        stack.push (12);
-        stack.push (18);
-        stack.print ();
-        stack.pop ();
-        stack.peek ();
-        stack.pop ();
-        stack.push (-5);
-        stack.print ();
+        Scanner sc = new Scanner(System.in);
+        Stack06 stk = new Stack06(5);
+        
+        char pilih;
+        do {
+            System.out.print("Jenis : ");
+            String jenis = sc.nextLine();
+            System.out.print("Warna : ");
+            String warna = sc.nextLine();
+            System.out.print("Merk : ");
+            String merk = sc.nextLine();
+            System.out.print("Ukuran : ");
+            String ukuran = sc.nextLine();
+            System.out.print("Harga : ");
+            double harga = sc.nextDouble();
+
+            Pakaian06 p = new Pakaian06 (jenis, warna, merk, ukuran, harga);
+            System.out.print("Apakah anda akan menambahkan data bau ke stack (y/n)? ");
+            pilih = sc.next() .charAt(0);
+            sc.nextLine();
+            stk.push(p);
+        } while (pilih == 'y');
+
+       stk.print();
+       stk.pop();
+       stk.peek();
+       stk.print();
     }
 }
